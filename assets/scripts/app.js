361e6ef6c03f05d29dfbcce77806cddec6ed17ec
Md. Tajul Islam <tajul.azazil@gmail.com>
2023-03-24 16:38:31 +0600

    ok

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,12 @@
 if("serviceWorker"in navigator){window.addEventListener("load",function(){navigator.serviceWorker.register("/serviceWorker.js").then(res=>console.log("service worker registered")).catch(err=>console.log("service worker not registered",err));});}
+
+
+    $(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
